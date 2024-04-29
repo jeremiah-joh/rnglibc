35fc9f92a2da47445c2aa12b088f8dd75624b129
Woohyun Joh <jeremiahjoh@sungkyul.ac.kr>
2024-04-29 23:17:06 +0900

    test: change sequence of code to check initialization

--- test.c
+++ test.c
@@ -6,13 +6,13 @@ main()
 {
 	size_t i;
 
-	puts("# osrng");
-	for (i = 0; i < 10; i++)
-		printf("%lu\n", osrng());
-
 	puts("# prng");
 	for (i = 0; i < 10; i++)
 		printf("%lu\n", prng());
+
+	puts("# osrng");
+	for (i = 0; i < 10; i++)
+		printf("%lu\n", osrng());
 
 	return 0;
 }
