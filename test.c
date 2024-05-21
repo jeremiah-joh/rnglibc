0cfaeb68731478e9d00a65207fa909b7cdb00fcf
Woohyun Joh <jeremiahjoh@sungkyul.ac.kr>
2024-05-21 13:21:03 +0900

    test: *_buf() are tested

--- test.c
+++ test.c
@@ -1,18 +1,30 @@
-#include "rng.h"
+#include "src/rng.h"
 #include <stdio.h>
 
 int
 main()
 {
-	size_t i;
+	size_t i, r;
 
-	puts("# prng");
-	for (i = 0; i < 10; i++)
+	puts("# prng()");
+	for (i = 0; i < 8; i++)
 		printf("%lu\n", prng());
 
-	puts("# osrng");
-	for (i = 0; i < 10; i++)
+	puts("# osrng()");
+	for (i = 0; i < 8; i++)
 		printf("%lu\n", osrng());
+
+	puts("# prng_buf()");
+	for (i = 0; i < 8; i++) {
+		prng_buf(&r, sizeof(r));
+		printf("%lu\n", r);
+	}
+
+	puts("# osrng_buf()");
+	for (i = 0; i < 8; i++) {
+		osrng_buf(&r, sizeof(r));
+		printf("%lu\n", r);
+	}
 
 	return 0;
 }
