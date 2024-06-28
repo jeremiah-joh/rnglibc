5e2b3d74b737bc9fa58f76a0894ee4ad6adca79f
Woohyun Joh <jeremiahjoh@sungkyul.ac.kr>
2024-06-28 16:54:57 +0900

    fix: reformat license text

--- src/rng.c
+++ src/rng.c
@@ -3,9 +3,9 @@
  * 
  * Written in 2024 by Woohyun Joh <jeremiahjoh@sungkyul.ac.kr>
  * 
- * To the extent possible under law, the author(s) have dedicated all
- * copyright and related and neighboring rights to this software to the
- * public domain worldwide. This software is distributed without any warranty.
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty.
  * 
  * You should have received a copy of the CC0 Public Domain Dedication along
  * with this software.
