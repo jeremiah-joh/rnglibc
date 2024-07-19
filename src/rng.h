949deb54ffa96d7dd9dedfbaf673c93cbacc3121
Woohyun Joh <jeremiahjoh@sungkyul.ac.kr>
2024-07-19 19:23:59 +0900

    fix: remove trailing spaces

--- src/rng.h
+++ src/rng.h
@@ -1,12 +1,12 @@
-/*   
+/*
  * rnglibc - useful random number generators written in ANSI C
- * 
+ *
  * Written in 2024 by Woohyun Joh <jeremiahjoh@sungkyul.ac.kr>
- * 
+ *
  * To the extent possible under law, the author(s) have dedicated all copyright
  * and related and neighboring rights to this software to the public domain
  * worldwide. This software is distributed without any warranty.
- * 
+ *
  * You should have received a copy of the CC0 Public Domain Dedication along
  * with this software.
  */
